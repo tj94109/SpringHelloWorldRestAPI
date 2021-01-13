85b9875839afd9f754bf104df1c0bf820c0d6d09
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-13 06:05:04 -0800

    Initial commit

---
+++ src/main/java/com/terrance/SpringHelloWorldRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.terrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terrance/SpringHelloWorldRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
